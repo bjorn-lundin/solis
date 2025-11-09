d29bd57b016c5b13b83401c4bcf3ef1e2259734a
bnl on prod <b.f.lundin@gmail.com>
2025-11-09 13:34:14 +0100

    fixed loop over data

--- source/ada/local/utils/json_extracter.adb
+++ source/ada/local/utils/json_extracter.adb
@@ -135,7 +135,7 @@ procedure Json_Extracter is
 --        Log ("-----------------------");
      end loop;
 
-     Log ("Start_Line,End_Line :" & Start_Line'img & End_Line'img);
+ --    Log ("Start_Line,End_Line :" & Start_Line'img & End_Line'img);
      
      for i in start_Line .. End_Line loop
        log (i'img & To_String(V(i)));
@@ -163,10 +163,10 @@ procedure Json_Extracter is
          null; -- has unicode characters we'd like to skip
        end if;
      end loop;  
-     Log ("Data_Holder :" & To_String(Data_Holder));
+   --  Log ("Data_Holder :" & To_String(Data_Holder));
 
      J := Read (Strm => To_String(Data_Holder), Filename => "");
-     Log ("J :" & J.Write);
+   --  Log ("J :" & J.Write);
       
       Data_Items := Get (J); --convert to array
       for I in 1 .. Length (Data_Items) loop
@@ -225,14 +225,15 @@ procedure Json_Extracter is
         end if;
 
         solis_data.read(eos);
-        if eos then
-          solis_data.insert;
+        if Eos
+          and then solis_data.Epoch > 0
+        then
+         solis_data.insert;
         else
           solis_data.update_withcheck;
         end if;
-
       end loop;
-  
+
    end On_Solis_Data;
    -----------------------------------------------------
 
@@ -258,74 +259,83 @@ procedure Json_Extracter is
       SMHI_Data         : table_smhi.data_type;
       Eos : Boolean := false;
       Epoch : Natural := 0;
+      use Calendar2;
    begin
       J := Read (Strm => Data, Filename => "");
       if J.Has_Field ("timeSeries") then
          Time_Series_Array := J.Get ("timeSeries");
+
          for I in 1 .. Length (Time_Series_Array) loop
-            Log ("we have result #:" & I'Img);
-            Data_Item := Get (Time_Series_Array, I);
-            SMHI_Data := table_smhi.Empty_Data;
+            for k in 1 .. 12 loop
+               Log ("we have result #:" & I'Img);
+               Data_Item := Get (Time_Series_Array, I);
+               SMHI_Data := table_smhi.Empty_Data;
 
-            if Data_Item.Has_Field ("time") then
-               declare
-                  tm : string := Data_Item.Get ("time");
-               begin
-                  SMHI_Data.time_start := Calendar2.To_Time_Type (tm(1..19) & ".000");
-                  Log ("time " & Tm);
-               end;
-            end if;
-
-            if Data_Item.Has_Field ("intervalParametersStartTime") then
-               declare
-                  ipst : string := Data_Item.Get ("intervalParametersStartTime");
-               begin
-                  SMHI_Data.time_stop := Calendar2.To_Time_Type (ipst(1..19) & ".000");
-                  Log ("intervalParametersStartTime " & ipst);
-                  Map_To_Epoch (SMHI_Data.time_start, Epoch);
-                  SMHI_Data.epoch := Integer_4(Epoch);
-               end;
-            end if;
-
-            if Data_Item.Has_Field ("data") then
-               Data_Item := Data_Item.Get ("data");
-               if Data_Item.Has_Field ("air_temperature") then
+               if Data_Item.Has_Field ("time") then
                   declare
-                     air_temperature : float :=
-                       Data_Item.Get ("air_temperature");
+                     tm : string := Data_Item.Get ("time");
                   begin
-                     SMHI_Data.air_temperature := air_temperature;
-                     Log ("air_temperature: " & air_temperature'image);
+                     SMHI_Data.time_stop := Calendar2.To_Time_Type (tm(1..19) & ".000");
+                     Log ("time " & Tm);
                   end;
                end if;
 
-               if Data_Item.Has_Field ("wind_speed") then
+               if Data_Item.Has_Field ("intervalParametersStartTime") then
                   declare
-                     wind_speed : float := Data_Item.Get ("wind_speed");
+                     ipst : string := Data_Item.Get ("intervalParametersStartTime");
                   begin
-                     SMHI_Data.wind_speed := wind_speed;
-                     Log ("wind_speed: " & wind_speed'image);
+                     SMHI_Data.time_start := Calendar2.To_Time_Type (ipst(1..19) & ".000");
+                    -- Log ("k " & k'img);
+                     SMHI_Data.time_start := SMHI_Data.time_start - (0, 0, Minute_Type((k-1)*5), 0, 0); -- every 5 minutes
+                     Log ("intervalParametersStartTime " & ipst);
+                     Map_To_Epoch (SMHI_Data.time_start, Epoch);
+                     SMHI_Data.epoch := Integer_4(Epoch);
+                     SMHI_Data.time_start := Calendar2.To_Time_Type (ipst(1..19) & ".000"); --reset
                   end;
                end if;
 
-               if Data_Item.Has_Field ("cloud_area_fraction") then
-                  declare
-                     Cloud_Coverage : Long_Long_Integer :=
-                       Data_Item.Get ("cloud_area_fraction");
-                  begin
-                     SMHI_Data.Cloud_Coverage := Types.Integer_4(Cloud_Coverage);
-                     Log("cloud_area_fraction: " & Cloud_Coverage'image);
-                  end;
+               if Data_Item.Has_Field ("data") then
+                  Data_Item := Data_Item.Get ("data");
+                  if Data_Item.Has_Field ("air_temperature") then
+                     declare
+                        air_temperature : float :=
+                        Data_Item.Get ("air_temperature");
+                     begin
+                        SMHI_Data.air_temperature := air_temperature;
+                        Log ("air_temperature: " & air_temperature'image);
+                     end;
+                  end if;
+
+                  if Data_Item.Has_Field ("wind_speed") then
+                     declare
+                        wind_speed : float := Data_Item.Get ("wind_speed");
+                     begin
+                        SMHI_Data.wind_speed := wind_speed;
+                        Log ("wind_speed: " & wind_speed'image);
+                     end;
+                  end if;
+
+                  if Data_Item.Has_Field ("cloud_area_fraction") then
+                     declare
+                        Cloud_Coverage : Long_Long_Integer :=
+                        Data_Item.Get ("cloud_area_fraction");
+                     begin
+                        SMHI_Data.Cloud_Coverage := Types.Integer_4(Cloud_Coverage);
+                        Log("cloud_area_fraction: " & Cloud_Coverage'image);
+                     end;
+                  end if;
                end if;
-            end if;
 
-            SMHI_Data.Read(eos);
-            if eos then
-              SMHI_Data.Insert;
-            else
-              SMHI_Data.Update_Withcheck;
-            end if;
+               SMHI_Data.Read(Eos);
+               if Eos
+                 and then SMHI_Data.Epoch > 0 
+               then
+                 SMHI_Data.Insert;
+               else
+                 SMHI_Data.Update_Withcheck;
+               end if;
 
+            end loop;
          end loop;
       end if;
    end on_SMHI_Data;
@@ -337,6 +347,7 @@ procedure Json_Extracter is
       Price_Data      : Table_Prices.Data_Type;
       Eos : Boolean := false;
       Epoch : Natural := 0;
+      use Calendar2;
 
 --[
 --{"SEK_per_kWh":0.05878,"EUR_per_kWh":0.00532,"EXR":11.04933,"time_start":"2025-10-12T00:00:00+02:00","time_end":"2025-10-12T00:15:00+02:00"},
@@ -348,65 +359,72 @@ procedure Json_Extracter is
       J := Read (Strm => Data, Filename => "");
       data_items := Get (J); --convert to array
 
+
       for I in 1 .. Length (data_items) loop
-         data_item := Get (data_items, I);
+         for k in 1 .. 4 loop  -- each 15 min divided into 4 parts of 5 min
+            data_item := Get (data_items, I);
 
-         Price_Data := Table_Prices.Empty_Data; 
-         
-         if data_item.Has_Field ("SEK_per_kWh") then
-            declare
-               sek : float:= data_item.Get ("SEK_per_kWh");
-            begin
-               Price_Data.SEK_per_kWh := sek;
-               Log ("SEK_per_kWh "& sek'img);
-            end;
-         end if;
+            Price_Data := Table_Prices.Empty_Data; 
+            
+            if data_item.Has_Field ("SEK_per_kWh") then
+               declare
+                  sek : float:= data_item.Get ("SEK_per_kWh");
+               begin
+                  Price_Data.SEK_per_kWh := sek;
+                  Log ("SEK_per_kWh "& sek'img);
+               end;
+            end if;
 
-         if data_item.Has_Field ("Eur_per_kWh") then
-            declare
-               eur : float:= data_item.Get ("Eur_per_kWh");
-            begin
-               Price_Data.Eur_per_kWh := eur;
-               Log ("Eur_per_kWh "& eur'img);
-            end;
-         end if;
+            if data_item.Has_Field ("Eur_per_kWh") then
+               declare
+                  eur : float:= data_item.Get ("Eur_per_kWh");
+               begin
+                  Price_Data.Eur_per_kWh := eur;
+                  Log ("Eur_per_kWh "& eur'img);
+               end;
+            end if;
 
-         if data_item.Has_Field ("EXR") then
-            declare
-               exr : float:= data_item.Get ("EXR");
-            begin
-               Price_Data.Exchange_Rate := exr;
-               Log ("EXR "& exr'img);
-            end;
-         end if;
+            if data_item.Has_Field ("EXR") then
+               declare
+                  exr : float:= data_item.Get ("EXR");
+               begin
+                  Price_Data.Exchange_Rate := exr;
+                  Log ("EXR "& exr'img);
+               end;
+            end if;
 
-         if data_item.Has_Field ("time_start") then
-            declare
-               ts : string:= data_item.Get ("time_start");
-            begin
-               Price_Data.Time_Start := Calendar2.To_Time_Type (ts(1..19) & ".000");
-               Log ("time_start" & ts);
-               Map_To_Epoch (Price_Data.Time_Start, Epoch);
-               Price_Data.epoch := Integer_4(Epoch);
-            end;
-         end if;
+            if data_item.Has_Field ("time_start") then
+               declare
+                  ts : string:= data_item.Get ("time_start");
+               begin
+                  Price_Data.Time_Start := Calendar2.To_Time_Type (ts(1..19) & ".000");
+                  Log ("time_start i" & I'img & " k" & k'img & " " & ts);
+                  Price_Data.Time_Start := Price_Data.Time_Start - (0, 0, Minute_Type((k-1)*15), 0, 0); -- every 15 minutes
+                  Map_To_Epoch (Price_Data.Time_Start, Epoch);
+                  Price_Data.epoch := Integer_4(Epoch);
+                  Price_Data.Time_Start := Calendar2.To_Time_Type (ts(1..19) & ".000"); --reset
+               end;
+            end if;
 
-         if data_item.Has_Field ("time_end") then
-            declare
-               te : string:= data_item.Get ("time_end");
-            begin
-               Price_Data.Time_Stop := Calendar2.To_Time_Type (te(1..19) & ".000");
-               Log ("time_end " & te);
-            end;
-         end if;
+            if data_item.Has_Field ("time_end") then
+               declare
+                  te : string:= data_item.Get ("time_end");
+               begin
+                  Price_Data.Time_Stop := Calendar2.To_Time_Type (te(1..19) & ".000");
+                  Log ("time_end " & te);
+               end;
+            end if;
 
-         Price_Data.read(eos);
-         if eos then
-            Price_Data.insert;
-         else
-            Price_Data.update_withcheck;
-         end if;         
+            Price_Data.Read(Eos);
+            if Eos
+               and then Price_Data.Epoch > 0
+            then
+               Price_Data.Insert;
+            else
+               Price_Data.Update_Withcheck;
+            end if;
 
+         end loop;
       end loop;
 
 --      end if;
@@ -463,16 +481,20 @@ begin
 
       declare
          s_name  : string := Simple_Name (Dir_Ent);
-         content : String := File_Content (Full_Name (Dir_Ent));
+         Full_Name : string := Full_Name (Dir_Ent);
+         content : String := File_Content (Full_Name);
       begin
          if s_name (1 .. 5) = "solis" then
             On_Solis_Data (content);
+            Ada.Directories.Move (Full_Name, "/home/bnl/solis/treated/" & s_name);
 
          elsif s_name (1 .. 4) = "smhi" then
             On_SMHI_Data (content);
+            Ada.Directories.Move (Full_Name, "/home/bnl/solis/treated/" & s_name);
 
          elsif s_name (1 .. 8) = "elpriser" then
             On_Elpriser_Data (content);
+            Ada.Directories.Move (Full_Name, "/home/bnl/solis/treated/" & s_name);
 
          else
             Text_io.put_line ("unhandled file " & Full_Name (Dir_Ent));
